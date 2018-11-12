4698597834e42e25fc5b888e7c9a6a767a8ea680
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-12 17:06:20 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,8 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
       t.string :name
-      add_foreign_key :show_id
-      add_foreign_key :actor_id
+      t.integer t.integer :show_id
     end
   end
 end
